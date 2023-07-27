1724ce5b56a8b46aba04101dcc19536b6372da2b
samantha uring <samanthauring@hotmail.com>
2023-07-27 19:07:47 -0300

    modificaciones

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var session = require('express-session');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -20,12 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(session({
-  secret: 'cursotn',
-  resave: false,
-  saveUninitialized: true
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
