19af6d2523dc7f344b6f17813839e8a46decf2a5
samantha uring <samanthauring@hotmail.com>
2023-08-15 21:26:42 -0300

    modificaciones

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
+
 var util = require('util');
 var cloudinary = require('cloudinary').v2;
-
 const uploader = util.promisify(cloudinary.uploader.upload);
 const destroy = util.promisify(cloudinary.uploader.destroy);
 
 
 router.get('/', async function (req, res, next) {
+
     var novedades = await novedadesModel.getNovedades();
 
     novedades = novedades.map(novedad => {
